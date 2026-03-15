6d498f19cb6fc3e9bdf55f06ace189a63f43919a
sirasu <sirasu270@gmail.com>
2026-03-15 15:34:35 +0900

    feat: user usecaseを実装

---
+++ backend/internal/usecase/login.go
@@ -0,0 +1,61 @@
+package usecase
+
+import (
+	"context"
+	"errors"
+
+	"github.com/S-s-dev-team/wAI/internal/domain"
+	"gorm.io/gorm"
+)
+
+type Login struct {
+	authRepository domain.AuthRepository
+	userRepository domain.UserRepository
+}
+
+func NewLogin(authRepository domain.AuthRepository, userRepository domain.UserRepository) *Login {
+	return &Login{
+		authRepository: authRepository,
+		userRepository: userRepository,
+	}
+}
+
+type LoginInput struct {
+	IDToken string
+}
+
+type LoginOutput struct {
+	User *domain.User
+}
+
+func (u *Login) Execute(ctx context.Context, input LoginInput) (*LoginOutput, error) {
+	tokenInfo, err := u.authRepository.VerifyIDToken(ctx, input.IDToken)
+	if err != nil {
+		return nil, err
+	}
+
+	user, err := u.userRepository.GetByFirebaseUID(ctx, tokenInfo.UID)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		user, err = u.userRepository.Create(ctx, &domain.User{
+			FirebaseUID: tokenInfo.UID,
+			Email:       tokenInfo.Email,
+			DisplayName: tokenInfo.DisplayName,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return &LoginOutput{User: user}, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	
+	user.Email = tokenInfo.Email
+	user.DisplayName = tokenInfo.DisplayName
+	user, err = u.userRepository.Update(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+
+	return &LoginOutput{User: user}, nil
+}
