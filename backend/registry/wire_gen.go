cf746df7c17afafeb260dbc037b5271485c7f831
sirasu <sirasu270@gmail.com>
2026-03-16 23:03:11 +0900

    feat: wire gen

--- backend/registry/wire_gen.go
+++ backend/registry/wire_gen.go
@@ -34,18 +34,18 @@ func InitializeApp(ctx context.Context) (*App, error) {
 	}
 	userRepository := repository.NewUser(db)
 	login := usecase.NewLogin(authRepository, userRepository)
-	handlerLogin := handler.NewLogin(login)
-	health := usecase.NewHealth()
-	handlerHealth := handler.NewHealth(health)
+	chatRepository := repository.NewChat(db)
+	chatUsecase := usecase.NewChatUsecase(chatRepository)
+	server := handler.NewServer(authRepository, userRepository, login, chatUsecase)
 	genaiClient, err := GeminiAPIProvider(ctx, vars)
 	if err != nil {
 		return nil, err
 	}
 	app := &App{
-		LoginHandler:  handlerLogin,
-		HealthHandler: handlerHealth,
-		DB:            db,
-		GenAI:         genaiClient,
+		Config: vars,
+		Server: server,
+		DB:     db,
+		GenAI:  genaiClient,
 	}
 	return app, nil
 }
@@ -53,8 +53,8 @@ func InitializeApp(ctx context.Context) (*App, error) {
 // wire.go:
 
 type App struct {
-	LoginHandler  *handler.Login
-	HealthHandler *handler.Health
-	DB            *gorm.DB
-	GenAI         *genai.Client
+	Config *config.Vars
+	Server *handler.Server
+	DB     *gorm.DB
+	GenAI  *genai.Client
 }
